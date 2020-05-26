487f2369d7e135ef0c5ff9b7597a39d419ed773a
dainius <dainius1999@hotmail.lt>
2020-05-26 10:10:22 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace algorithims_lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
